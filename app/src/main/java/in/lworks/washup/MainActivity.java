0c141d218400242ea9e5d0b0a632c804f106da71
Saikiran Pulluri <psk.knr@gmail.com>
2015-09-29 00:12:37 +0530

    Home Page Tabs Updated

--- app/src/main/java/in/lworks/washup/MainActivity.java
+++ app/src/main/java/in/lworks/washup/MainActivity.java
@@ -1,31 +1,62 @@
 package in.lworks.washup;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import tabs.SlidingTabLayout;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
+    private ViewPager mPager;
+    private SlidingTabLayout mTabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar=(Toolbar)findViewById(R.id.app_bar);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
-        if(getSupportActionBar()!=null) {
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
-        NavigationDrawerFragment drawerFragment=(NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
-        drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout),toolbar);
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        mTabs = (SlidingTabLayout) findViewById(R.id.gender_selection_tab);
+        mTabs.setDistributeEvenly(true);
+
+        mTabs.setCustomTabView(R.layout.custom_tab_view, R.id.tabText);
+        mTabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.accentColor);
+            }
+        });
+        mTabs.setViewPager(mPager);
     }
 
     @Override
@@ -44,10 +75,67 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_cart) {
-            startActivity(new Intent(this,CartActivity.class));
+            startActivity(new Intent(this, CartActivity.class));
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class MyFragment extends Fragment {
+
+        private TextView textView;
+
+        public static MyFragment getInstance(int position) {
+            MyFragment myFragment = new MyFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", position);
+            myFragment.setArguments(args);
+            return myFragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View layout = inflater.inflate(R.layout.washtype_fragment, container, false);
+            textView = (TextView) layout.findViewById(R.id.position);
+            Bundle bundle=getArguments();
+            if(bundle!=null){
+                textView.setText(bundle.getInt("position")+"");
+            }
+
+            return layout;
+        }
+    }
+
+    class MyPagerAdapter extends FragmentPagerAdapter {
+
+        int icons[]={R.drawable.ic_men,R.drawable.ic_men,R.drawable.ic_men,R.drawable.ic_men};
+        String[] tabs;
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+            tabs=getResources().getStringArray(R.array.tabs);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            Drawable drawable=getResources().getDrawable(icons[position]);
+            drawable.setBounds(0,0,36,36);
+            ImageSpan imageSpan=new ImageSpan(drawable);
+            SpannableString spannableString=new SpannableString(tabs[position]);
+            spannableString.setSpan(imageSpan,0,spannableString.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+            return spannableString;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            MyFragment myFragment=MyFragment.getInstance(position);
+            return myFragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
     }
 }
