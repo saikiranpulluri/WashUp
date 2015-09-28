7296c7dd17f4f493fd65dcc5dc5340c4871890c1
Saikiran Pulluri <psk.knr@gmail.com>
2015-09-28 18:55:11 +0530

    Sectioned Navigation Drawer

--- app/src/main/java/adapter/NavAdapter.java
+++ app/src/main/java/adapter/NavAdapter.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -18,44 +17,85 @@ import in.lworks.washup.R;
 /**
  * Created by Saikiran Pulluri on 26-09-2015.
  */
-public class NavAdapter extends RecyclerView.Adapter<NavAdapter.NavViewHolder> {
+public class NavAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int TYPE_HEADER=0;
+    private static final int TYPE_ITEM=1;
 
     private LayoutInflater inflater;
     List<NavInformation> data= Collections.emptyList();
+    private Context context;
 
     public NavAdapter(Context context,List<NavInformation> data) {
+        this.context=context;
         inflater = LayoutInflater.from(context);
         this.data=data;
     }
 
     @Override
-    public NavViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View view = inflater.inflate(R.layout.custom_row, viewGroup, false);
-        NavViewHolder viewHolder=new NavViewHolder(view);
-        return viewHolder;
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        if(viewType==TYPE_HEADER){
+            View view = inflater.inflate(R.layout.drawer_header, viewGroup, false);
+            HeaderHolder viewHolder=new HeaderHolder(view);
+            return viewHolder;
+        }else {
+            View view = inflater.inflate(R.layout.custom_row, viewGroup, false);
+            NavViewHolder viewHolder=new NavViewHolder(view);
+            return viewHolder;
+        }
     }
 
     @Override
-    public void onBindViewHolder(NavViewHolder viewHolder, int position) {
-        NavInformation currentData=data.get(position);
-        viewHolder.title.setText(currentData.title);
-        viewHolder.icon.setImageResource(currentData.itemId);
+    public int getItemViewType(int position) {
+        if(position==0){
+            return TYPE_HEADER;
+        }else {
+            return TYPE_ITEM;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder,int position) {
+        if(viewHolder instanceof HeaderHolder){
+
+        }else {
+            NavViewHolder holder=(NavViewHolder)viewHolder;
+            NavInformation currentData = data.get(position-1);
+            holder.title.setText(currentData.title);
+            holder.icon.setImageResource(currentData.itemId);
+       }
     }
 
     @Override
     public int getItemCount() {
-        return data.size();
+        return data.size()+1;
     }
 
-    class NavViewHolder extends RecyclerView.ViewHolder {
+    class NavViewHolder extends RecyclerView.ViewHolder{
 
         TextView title;
         ImageView icon;
 
         public NavViewHolder(View itemView) {
             super(itemView);
+            //itemView.setOnClickListener(this);
             title=(TextView)itemView.findViewById(R.id.listCaption);
             icon=(ImageView)itemView.findViewById(R.id.listIcon);
+        }
+
+//        @Override
+//        public void onClick(View v) {
+//            context.startActivity(new Intent(context, AddressesActivity.class));
+//        }
+    }
+
+    class HeaderHolder extends RecyclerView.ViewHolder{
+
+        TextView title;
+
+        public HeaderHolder(View itemView) {
+            super(itemView);
+            title=(TextView)itemView.findViewById(R.id.nav_header_text);
         }
     }
 }
--- app/src/main/java/in/lworks/washup/NavigationDrawerFragment.java
+++ app/src/main/java/in/lworks/washup/NavigationDrawerFragment.java
@@ -10,9 +10,12 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -66,6 +69,17 @@ public class NavigationDrawerFragment extends Fragment {
         adapter=new NavAdapter(getActivity(),getData());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(), recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(getActivity(),"OnClick",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
         return layout;
     }
 
@@ -119,6 +133,41 @@ public class NavigationDrawerFragment extends Fragment {
         });
     }
 
+    class RecyclerTouchListener implements RecyclerView.OnItemTouchListener{
+        private GestureDetector gestureDetector;
+
+        private ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener) {
+            this.clickListener=clickListener;
+            gestureDetector=new GestureDetector(context, new GestureDetector.SimpleOnGestureListener(){
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                   return true;
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            View child=rv.findChildViewUnder(e.getX(),e.getY());
+            if(child!=null&&clickListener!=null&&gestureDetector.onTouchEvent(e)){
+                clickListener.onClick(child,rv.getChildPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
+
     public static void saveToPreference(Context context,String preferenceName,String preferenceValue){
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME,Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -129,5 +178,10 @@ public class NavigationDrawerFragment extends Fragment {
     public static String readFromPreferences(Context context,String preferenceName,String defaultValue){
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME,Context.MODE_PRIVATE);
         return sharedPreferences.getString(preferenceName,defaultValue);
+    }
+
+    public static interface ClickListener{
+        public void onClick(View view,int position);
+        public void onLongClick(View view,int position);
     }
 }
